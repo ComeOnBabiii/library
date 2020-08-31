04650011d38167027298d62684e0d5ea6d65f046
ComeOnBabiii <duccc0926@gmail.com>
2020-08-31 10:50:45 +0700

    generate project

---
+++ src/main/java/com/abc/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.abc.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.abc.model.Tutorial;
+
+public interface  TutorialRepository extends JpaRepository<Tutorial, Long>{
+	List<Tutorial> findByPublished(boolean published);
+	List<Tutorial> findByTitleContaining(String title);
+}
